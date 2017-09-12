0cc41a723d4b9e900079ff1e77c76afa1207de13
Farki93 <cph-if36@cphbusiness.com>
2017-09-12 13:54:29 +0200

    add facade, sql

---
+++ src/main/java/facade/Facade.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import entity.CityInfo;
+import entity.Hobby;
+import entity.Person;
+import java.util.List;
+
+/**
+ *
+ * @author AR
+ */
+public class Facade implements IFacade{
+
+    @Override
+    public Person getPersonById(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Person getPersonByPhoneNumber(String phoneNumber) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> getPersons() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> getPersonsByZipCode(int zipCode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> getPersonByCity(CityInfo city) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> getPersonsByHobby(Hobby hobby) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Person createPerson(Person person) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Person editPerson(long id, Person person) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Person deletePerson(Person person) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/main/java/facade/IFacade.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+import entity.*;
+import java.util.List;
+/**
+ *
+ * @author AR
+ */
+public interface IFacade {
+    
+    public Person getPersonById(long id);
+    public Person getPersonByPhoneNumber(String phoneNumber);
+    public List<Person> getPersons();
+    public List<Person> getPersonsByZipCode(int zipCode);
+    public List<Person> getPersonByCity(CityInfo city);
+    public List<Person> getPersonsByHobby(Hobby hobby);
+    public Person createPerson(Person person);
+    public Person editPerson(long id, Person person);
+    public Person deletePerson(Person person);
+    
+    
+}
