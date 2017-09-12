c28fceecc6c7737ebeeca01be5c8bc1efec16fcb
Jakob <jakob>
2017-09-12 10:21:53 +0200

    Address and CityInfo entities

--- src/main/java/entity/Address.java
+++ src/main/java/entity/Address.java
@@ -1,25 +1,49 @@
-package ca1;
+package entity;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Address implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    private String street;
+    
+    private String additionalInfo;
 
+    @ManyToOne
+    private String zip;
+    
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
     }
 
     @Override
@@ -44,7 +68,7 @@ public class Address implements Serializable {
 
     @Override
     public String toString() {
-        return "ca1.Address[ id=" + id + " ]";
+        return "Address{" + "id=" + id + ", street=" + street + ", additionalInfo=" + additionalInfo + '}';
     }
 
 }
---
+++ src/main/java/entity/CityInfo.java
@@ -0,0 +1,68 @@
+package entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CityInfo implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private String zip;
+
+    private String city;
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + Objects.hashCode(this.zip);
+        hash = 71 * hash + Objects.hashCode(this.city);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CityInfo other = (CityInfo) obj;
+        if (!Objects.equals(this.zip, other.zip)) {
+            return false;
+        }
+        if (!Objects.equals(this.city, other.city)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CityInfo{" + "zip=" + zip + ", city=" + city + '}';
+    }
+}
