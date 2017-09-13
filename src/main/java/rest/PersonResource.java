62b57780c1449d2617d2928f53921ada2fc489ce
Jakob <jakob>
2017-09-13 14:35:53 +0200

    Implement POST /api/person

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -1,16 +1,23 @@
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entity.Person;
+import facade.Facade;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 
 @Path("person")
 public class PersonResource {
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static Facade facade = new Facade();
 
     @Context
     private UriInfo context;
@@ -32,12 +39,19 @@ public class PersonResource {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * PUT method for updating or creating an instance of GenericResource
-     * @param content representation for the resource
-     */
-    @PUT
+    @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    public String postPerson(String content) {
+        Person person = gson.fromJson(content, Person.class);
+        if (person == null) {
+            return gson.toJson(false);
+        }
+        facade.createPerson(person);
+        return gson.toJson(true);
+        /*Person p1 = new Person("bob@gmail.com", "Bob", "Jensen");
+        p1 = gson.fromJson(content, Person.class);
+        return gson.toJson(p1);
+/*        return content;
+        return "true\n";*/
     }
 }
