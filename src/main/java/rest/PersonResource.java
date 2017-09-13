864e2c45fc3a30ccb1ef3f522930f145fb68ba4e
Maksymilian <a.maksymilian.p@gmail.com>
2017-09-13 13:35:18 +0200

    Behind Master

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -1,5 +1,9 @@
 package rest;
 
+import com.google.gson.Gson;
+import entity.Person;
+import facade.Facade;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -7,31 +11,39 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("person")
+@Path("person") 
 public class PersonResource {
+    private static Gson gson = new Gson(); 
+    private final static Facade facade = new Facade();
 
     @Context
     private UriInfo context;
 
-    /**
-     * Creates a new instance of GenericResource
-     */
     public PersonResource() {
     }
-
-    /**
-     * Retrieves representation of an instance of entity.GenericResource
-     * @return an instance of java.lang.String
-     */
+    
     @GET
+    @Path("/api/person/complete/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    public String getPerson(@PathParam("id") long id) { 
+        Person person = facade.getPerson(id);
+        String text = gson.toJson(person);
+        return text;     
     }
-
+    
+    @GET
+    @Path("/api/person/complete/{all}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPersons() { 
+        List<Person> persons = facade.getPersons();
+        String text1 = gson.toJson(persons);
+        return text1; 
+    }
+    
     /**
      * PUT method for updating or creating an instance of GenericResource
      * @param content representation for the resource
