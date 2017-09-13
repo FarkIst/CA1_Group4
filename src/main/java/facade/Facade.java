df2bf610cd3fd26c9ea312ccd0efb7206576e10f
Jakob <jakob>
2017-09-13 13:20:05 +0200

    Make Facade create an EntityManager

--- src/main/java/facade/Facade.java
+++ src/main/java/facade/Facade.java
@@ -7,11 +7,13 @@ import entity.Person;
 import entity.Phone;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 public class Facade implements IFacade{
-
-    private EntityManager em;
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
+    private final EntityManager em = emf.createEntityManager();
 
     @Override
     public Person getPerson(long id) {
